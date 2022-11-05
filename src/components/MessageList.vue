75d3ecf4509ac6c6896b517a96da5d27e5383198
kifuan <kifuan@foxmail.com>
2022-11-05 14:09:27 +0800

    feat: skeleton

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { NList, NListItem } from 'naive-ui'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import type { MessageFetch } from '../api'
 import { api } from '../api'
 import { useMessageStore } from '../stores/message'
 import Message from './Message.vue'
+import MessageSkeleton from './MessageSkeleton.vue'
 
 const { reply } = withDefaults(defineProps<{
   reply?: number
@@ -14,19 +15,29 @@ const { reply } = withDefaults(defineProps<{
 
 const store = useMessageStore()
 const messages = computed(() => store.getRootReplies(reply))
+const loading = ref(true)
 
 api.get<MessageFetch[]>('messages').then((m) => {
   store.setMessages(m.data)
+  loading.value = false
 })
 </script>
 
 <template>
-  <NList v-if="messages" :show-divider="false">
-    <NListItem
-      v-for="m in messages"
-      :key="m.id"
-    >
-      <Message :message="m" />
-    </NListItem>
+  <NList :show-divider="false">
+    <template v-if="loading">
+      <NListItem v-for="i in 5" :key="i">
+        <MessageSkeleton />
+      </NListItem>
+    </template>
+
+    <template v-else>
+      <NListItem
+        v-for="m in messages"
+        :key="m.id"
+      >
+        <Message :message="m" />
+      </NListItem>
+    </template>
   </NList>
 </template>
---
+++ src/components/MessageSkeleton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { NSkeleton, NThing } from 'naive-ui'
+</script>
+
+<template>
+  <NThing content-indented>
+    <template #avatar>
+      <NSkeleton circle size="large" />
+    </template>
+
+    <NSkeleton text style="width: 30%" />
+    <NSkeleton :repeat="2" text />
+    <NSkeleton text style="width: 70%" />
+  </NThing>
+</template>
